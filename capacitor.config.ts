4d559ec53814f6be640212604b11c27539bf2ec9
ppepito13 <adres_smieciowy@outlook.com>
2026-03-23 13:50:17 +0100

    feat: Initialize Android platform with app name, icons, and splash screens.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.pepitolabs.generator',
-  appName: 'Nextn',
+  appName: 'Generator Treningów SW',
   webDir: 'out'
 };
 
